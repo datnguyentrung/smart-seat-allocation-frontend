06ece1c7ff26a6aa54c33cf699b8a44cf9ad2e89
Nguyen Trung Dat <datnguyentrung.ptit@gmail.com>
2026-02-27 15:47:43 +0700

    feat: add TicketModal component with QR code support and update dependencies

---
+++ src/components/TicketModal/TicketModal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {QRCodeSVG} from 'qrcode.react';
+import type {TicketResponse} from "@types/types";
+
+export default function TicketModal({ticket}: {ticket: TicketResponse}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
